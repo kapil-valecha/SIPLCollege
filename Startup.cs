71076c2a7d22fcc1048e7ddd2e26744cca442330
kapil-valecha <kapil.valecha@systematixindia.com>
2018-09-10 19:58:01 +0530

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SIPLCollege.Models;
+using System.Security.Claims;
+
+
+[assembly: OwinStartupAttribute(typeof(SIPLCollege.Startup))]
+namespace SIPLCollege
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User 
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website				
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "admin@1234";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role 
+            if (!roleManager.RoleExists("Teacher"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Teacher";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role 
+            if (!roleManager.RoleExists("Student"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Student";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
