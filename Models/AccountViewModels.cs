2f3e15e3515af52c20693982aee1f810b2553f53
kapil-valecha <kapil.valecha@systematixindia.com>
2018-09-11 18:58:29 +0530

    Updates 7:00 pm

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -77,9 +77,9 @@ namespace SIPLCollege.Models
         [Display(Name = "UserName")]
         public string UserName { get; set; }
 
-        [Required]
-        [Display(Name = "Student Profile Photo")]
-        public string StudentImage { get; set; }
+        //[Required]
+        //[Display(Name = "Student Profile Photo")]
+        //public string StudentImage { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
